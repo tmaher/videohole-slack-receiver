9069d6a59dc794ec6108564bf551023ae6f34f49
Tom Maher <tmaher@github.com>
2018-04-02 13:09:42 +0100

    wip

--- app.rb
+++ app.rb
@@ -12,7 +12,9 @@ def do_slack_hook(resp)
 end
 
 before(/./) do
-  halt 403 unless params[:token] == ENV['SLACK_SHARED_SECRET'] + "foo"
+  shared_secret = ENV['SLACK_SHARED_SECRET']
+  STDERR.puts "token is #{params[:token]}, secret #{shared_secret}"
+  halt 403 unless params[:token] == shared_secret
 end
 
 post '/late' do
