2834ab84f81d2a495cd6cb2766e887a49f380d3b
Tom Maher <tmaher@github.com>
2018-04-02 13:17:35 +0100

    wip

--- app.rb
+++ app.rb
@@ -12,10 +12,9 @@ def do_slack_hook(resp)
 end
 
 before do
-  STDERR.puts "I am a before block"
-  shared_secret = "#{ENV['SLACK_SHARED_SECRET']} others"
-  STDERR.puts "token is #{params[:token]}, secret #{shared_secret}"
-  halt 403 unless params[:token] == shared_secret
+  # shared_secret = "#{ENV['SLACK_SHARED_SECRET']} others"
+  # STDERR.puts "token is #{params[:token]}, secret #{shared_secret}"
+  halt 403 unless params[:token] == "#{ENV['SLACK_SHARED_SECRET']}foo"
 end
 
 post '/late' do
