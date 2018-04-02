8568949627bc3c10773309a1c903596fd8e50f57
Tom Maher <tmaher@github.com>
2018-04-02 14:24:30 +0100

    ssh

--- startup-ssh-wrap.sh
+++ startup-ssh-wrap.sh
@@ -4,5 +4,6 @@ eval $(ssh-agent -s)
 echo "${SERVER_SSH_PRIVATE_KEY}" > /tmp/server.key
 ssh-add /tmp/server.key
 rm /tmp/server.key
+unset SERVER_SSH_PRIVATE_KEY
 
 bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
